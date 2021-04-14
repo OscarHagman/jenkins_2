91fd6cab1aae012b8a56ffe7429d711d7e3cc900
tomten <tomten@pop-os.localdomain>
2021-04-14 16:02:57 +0200

    first commit

---
+++ launch_servers/launch_server.sh
@@ -0,0 +1,2 @@
+ansible-playbook launch_AWS_ubuntu_server.yml
+ansible-playbook -i webserver_ip.yml install_webserver.yml
---
+++ run.sh
@@ -0,0 +1,1 @@
+ansible-playbook -i launch_servers/webserver_ip.yml github_pipeline/deploy.yml
